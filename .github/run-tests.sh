caffae7ae3e1cc2e2f3fde9cc92b2ef122e7a24b
Steve Kemp <steve@steve.org.uk>
2020-04-14 10:17:32 +0300

    Added CI

---
+++ .github/run-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Install tools to test our code-quality.
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
+go get -u honnef.co/go/tools/cmd/staticcheck
+
+
+# Run the static-check tool.
+t=$(mktemp)
+staticcheck -checks all ./... > $t
+if [ -s $t ]; then
+    echo "Found errors via 'staticcheck'"
+    cat $t
+    rm $t
+    exit 1
+fi
+rm $t
+
+# At this point failures cause aborts
+set -e
+
+# Run the linter
+echo "Launching linter .."
+golint -set_exit_status ./...
+echo "Completed linter .."
+
+# Run the shadow-checker
+echo "Launching shadowed-variable check .."
+go vet -vettool=$(which shadow) ./...
+echo "Completed shadowed-variable check .."
