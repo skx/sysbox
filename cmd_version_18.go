c909b592e688ba86b8903b9c90cadd37cfe403db
Steve Kemp <steve@steve.org.uk>
2022-03-22 17:01:33 +0200

    Show more output on our "versione" sub-command
    This is conditional on go 1.18 being present

--- cmd_version_18.go
+++ cmd_version_18.go
@@ -1,10 +1,12 @@
-//go:build !go1.18
-// +build !go1.18
+//go:build go1.18
+// +build go1.18
 
 package main
 
 import (
 	"fmt"
+	"runtime/debug"
+	"strings"
 
 	"github.com/skx/subcommands"
 )
@@ -34,6 +36,16 @@ This reports upon the version of the application.
 func (t *versionCommand) Execute(args []string) int {
 
 	fmt.Printf("%s\n", version)
+
+	info, ok := debug.ReadBuildInfo()
+
+	if ok {
+		for _, settings := range info.Settings {
+			if strings.Contains(settings.Key, "vcs") {
+				fmt.Printf("%s: %s\n", settings.Key, settings.Value)
+			}
+		}
+	}
 
 	return 0
 }
