d07f87622ab318e967cebda4481438cf7469a8e4
Steve Kemp <steve@steve.org.uk>
2024-11-06 21:15:18 +0200

    Added 'rss' sub-command because it is easier than sed/awk
    This adds a simple command to show the links that are present
    in a given RSS feed, by URL.
    
    For example:
    
    ```
    $ sysbox rss https://blog.steve.fi/index.rss
    https://blog.steve.fi/the_cp_m_emulator_is_good_enough__i_think_.html
    https://blog.steve.fi/the_cp_m_emulator_is_working_well.html
    https://blog.steve.fi/the_cp_m_emulator_development_continues.html
    https://blog.steve.fi/a_simple_golang_cp_m_emulator.html
    https://blog.steve.fi/a_simple_package_for_running_many_linters.html
    https://blog.steve.fi/falsehoods_i_used_to_believe_about_shoes.html
    https://blog.steve.fi/please_to_meet_you__hope_you_guessed_my_name_.html
    https://blog.steve.fi/old_school_cgi_scripts_.html
    https://blog.steve.fi/simple_repl_for_cp_m__in_z80_assembly.html
    https://blog.steve.fi/simple_toy_languages.html
    ```

---
+++ cmd_rss.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/mmcdole/gofeed"
+)
+
+// Structure for our options and state.
+type rssCommand struct {
+	// format contains the format-string to display for entries.
+	// We recognize "$link" and "$title".  More might be added in the future.
+	format string
+}
+
+// Arguments adds per-command args to the object.
+func (r *rssCommand) Arguments(f *flag.FlagSet) {
+	f.StringVar(&r.format, "format", "$link", "Specify the format-string to display for entries")
+}
+
+// Info returns the name of this subcommand.
+func (r *rssCommand) Info() (string, string) {
+	return "rss", `Show details from an RSS feed.
+
+Details:
+
+This command fetches the specified URLs as RSS feeds, and shows
+their contents in a simple fashion.  By default only the entry URLs
+are shown, but a format-string may be used to specify the output.
+
+For example to show the link and title of entries:
+
+    $ sysbox rss -format='$link $title' http://,..
+
+Suggestions for additional fields/details to be displayed are
+welcome via issue-reports.
+
+
+Usage:
+
+   $ sysbox rss url1 url2 .. urlN
+
+Note:
+
+Care must be taken to escape, or quote, the '$' character which
+is used in the format-string.
+`
+}
+
+// Process each specified feed.
+func (r *rssCommand) processFeed(url string) error {
+
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
+	if err != nil {
+		return err
+	}
+
+	for _, ent := range feed.Items {
+		txt := os.Expand(
+			r.format,
+			func(s string) string {
+				switch s {
+				case "content":
+					return ent.Content
+				case "link":
+					return ent.Link
+				case "title":
+					return ent.Title
+				default:
+					return s
+				}
+			},
+		)
+
+		fmt.Println(txt)
+	}
+	return err
+}
+
+// Execute is invoked if the user specifies `rss` as the subcommand.
+func (r *rssCommand) Execute(args []string) int {
+
+	for _, u := range args {
+
+		err := r.processFeed(u)
+		if err != nil {
+			fmt.Printf("Failed to process %s: %s\n", u, err)
+		}
+	}
+
+	return 0
+}
--- go.mod
+++ go.mod
@@ -1,34 +1,41 @@
 module github.com/skx/sysbox
 
-go 1.21
+go 1.23
 
-toolchain go1.22.5
+toolchain go1.23.2
 
 require (
-	github.com/creack/pty v1.1.21
+	github.com/creack/pty v1.1.24
 	github.com/gdamore/tcell/v2 v2.7.4
 	github.com/google/goexpect v0.0.0-20210430020637-ab937bf7fd6f
+	github.com/mmcdole/gofeed v1.3.0
 	github.com/nightlyone/lockfile v1.0.0
 	github.com/peterh/liner v1.2.2
-	github.com/rivo/tview v0.0.0-20240807205129-e4c497cc59ed
+	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592
 	github.com/skx/subcommands v0.9.2
-	golang.org/x/net v0.28.0
-	golang.org/x/term v0.23.0
+	golang.org/x/net v0.30.0
+	golang.org/x/term v0.25.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.10.0 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/gdamore/encoding v1.0.1 // indirect
 	github.com/google/goterm v0.0.0-20200907032337-555d40f16ae2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mmcdole/goxpp v1.1.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240808171019-573a1156607a // indirect
-	google.golang.org/grpc v1.65.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241104194629-dd2ea8efbc28 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
